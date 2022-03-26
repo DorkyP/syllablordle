0caaeb5930e146650ba1c4fb05d00ae60564b4d0
DorkyP <64234510+DorkyP@users.noreply.github.com>
2022-03-26 17:21:34 +1100

    Fixed where solution was visible on modal before end of game

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -64,36 +64,38 @@ export const StatsModal = ({
         gameStats={gameStats}
         numberOfGuessesMade={numberOfGuessesMade}
       />
-			<h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
-				Define: <a href={`https://www.wordnik.com/words/`+solution.toLowerCase()} target="_blank" rel="noopener noreferrer">{solution}</a>
-      </h4>
       {(isGameLost || isGameWon) && (
-        <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
-          <div>
-            <h5>{NEW_WORD_TEXT}</h5>
-            <Countdown
-              className="text-lg font-medium text-gray-900 dark:text-gray-100"
-              date={tomorrow}
-              daysInHours={true}
-            />
-          </div>
-          <button
-            type="button"
-            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-            onClick={() => {
-              shareStatus(
-                guesses,
-                isGameLost,
-                isHardMode,
-                isDarkMode,
-                isHighContrastMode,
-                handleShareToClipboard
-              )
-            }}
-          >
-            {SHARE_TEXT}
-          </button>
-        </div>
+				<div>
+					<h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
+						Define: <a href={`https://www.wordnik.com/words/`+solution.toLowerCase()} target="_blank" rel="noopener noreferrer">{solution}</a>
+      		</h4>
+        	<div className="mt-5 sm:mt-6 columns-2 dark:text-white">
+						<div>
+							<h5>{NEW_WORD_TEXT}</h5>
+							<Countdown
+								className="text-lg font-medium text-gray-900 dark:text-gray-100"
+								date={tomorrow}
+								daysInHours={true}
+							/>
+						</div>
+						<button
+							type="button"
+							className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+							onClick={() => {
+								shareStatus(
+									guesses,
+									isGameLost,
+									isHardMode,
+									isDarkMode,
+									isHighContrastMode,
+									handleShareToClipboard
+								)
+							}}
+						>
+							{SHARE_TEXT}
+						</button>
+					</div>
+				</div>
       )}
     </BaseModal>
   )
